3ba9eca4f90a925e645551837f0987cc6c3019dc
NinaDavydenko <105279902+NinaDavydenko@users.noreply.github.com>
2023-06-15 18:30:05 +0100

    Add files via upload

---
+++ WineProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WineProject.Startup))]
+namespace WineProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
